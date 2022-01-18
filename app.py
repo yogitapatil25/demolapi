aa8fed514f83f1151435e9e572516dff880dec6f
yogitapatil25 <55626383+yogitapatil25@users.noreply.github.com>
2022-01-18 16:34:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+import pickle
+
+
+
+app = Flask(__name__)
+model = pickle.load(open('laptosell.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Laptop Price Predicition is $ {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
